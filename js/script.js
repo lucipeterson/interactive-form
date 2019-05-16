223d704d224cbff481582f9203050af4c3cfb6be
Luci Peterson <lucijpeterson@gmail.com>
2019-05-16 11:02:02 -0700

    Added totaling feature to disableConflictingActivities() and changed name
    Added totaling feature to disableConflictingActivities() function, added punctuation, and changed name of function to "chooseActivities()".  User can now see a running total cost based on what activities they are signed up for.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const heartColors = $("#color option:contains('I')");
 const tuesNineToTwelve = $(".activities label:contains('Tuesday 9am-12pm')");
 const tuesOneToFour = $(".activities label:contains('Tuesday 1pm-4pm')");
 const activitiesCheckboxes = $(".activities :checkbox");
+const registrationTotal = $(".activities").append('<div id = "total">');
 
 //PAGE DEFAULT SETTINGS
 name.focus();
@@ -29,26 +30,49 @@ function displayColorOptions(){
 };
 
 //Function that restricts user from selecting conflicting activities.
-function disableConflictingActivities(){
+function chooseActivities(){
+    let totalCost = 0;
     tuesNineToTwelve.find(':first-child').attr('class','tues-nine-twelve');
     tuesOneToFour.find(':first-child').attr('class','tues-one-four');
     activitiesCheckboxes.prop('checked', status).change(function(){
+        if (($(this).prop('checked') === true)) {
+            totalCost += 100; 
+            $('#total').html(`<h2>Your Total Cost: $${totalCost}.00</h2>`);
+        };
+        if (($(this).prop('checked') === true) 
+        && ($(this).prop('name') === 'all')) {
+            totalCost += 100; 
+            $('#total').html(`<h2>Your Total Cost: $${totalCost}.00</h2>`);
+        };
+        if (($(this).prop('checked') === false)) {
+            totalCost -= 100; 
+            $('#total').html(`<h2>Your Total Cost: $${totalCost}.00</h2>`);
+        };
+        if (($(this).prop('checked') === false) 
+        && ($(this).prop('name') === 'all')) {
+            totalCost -= 100; 
+            $('#total').html(`<h2>Your Total Cost: $${totalCost}.00</h2>`);
+        };
         if (($(this).prop('checked') === true) 
         && ($(this).prop('class') === 'tues-nine-twelve')){
             $('.tues-nine-twelve').prop('disabled', true);
-            $(this).prop('disabled', false);}
+            $(this).prop('disabled', false);
+        };
         if (($(this).prop('checked') === false) 
         && ($(this).prop('class') === 'tues-nine-twelve')){
             $('.tues-nine-twelve').prop('disabled', false);
-            $(this).prop('disabled', false);}
+            $(this).prop('disabled', false);
+        };
         if (($(this).prop('checked') === true) 
         && ($(this).prop('class') === 'tues-one-four')){
             $('.tues-one-four').prop('disabled', true);
-            $(this).prop('disabled', false);}
+            $(this).prop('disabled', false);
+        };
         if (($(this).prop('checked') === false) 
         && ($(this).prop('class') === 'tues-one-four')){
             $('.tues-one-four').prop('disabled', false);
-            $(this).prop('disabled', false);} 
+            $(this).prop('disabled', false);
+        };
     })
 };
 
@@ -66,6 +90,6 @@ function formValidation(){
 //FUNCTIONS INVOKED WHEN USER INPUT CHANGES
 $("#title").on("change", showJobRoleField);
 $("#design").on("change", displayColorOptions);
-disableConflictingActivities();
+chooseActivities();
 $("#payment").on("change", displayPaymentDetails);
 
